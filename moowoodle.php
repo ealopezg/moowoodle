ef518f234e22daf27a72ea59031c4443069521ed
Esteban López <esteban.lopez.g@usach.cl>
2023-01-19 19:58:07 -0300

    Test

--- moowoodle.php
+++ moowoodle.php
@@ -69,3 +69,11 @@ if ( ! class_exists( 'MooWoodle' ) && MooWoodle_Dependencies::woocommerce_active
 	$MooWoodle = new MooWoodle( __FILE__ );
 	$GLOBALS[ 'MooWoodle' ] = $MooWoodle;
 }
+
+/** Code to disable update */
+add_filter('site_transient_update_plugins', 'remove_update_notification');
+function remove_update_notification($value)
+{
+	unset($value->response[plugin_basename(__FILE__)]);
+	return $value;
+}
